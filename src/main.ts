16b7bce134e10f608734afee6d52377149231ec3
michael-bailey <mickyb18a@gmail.com>
2021-06-18 09:42:15 +0100

    Added handlebars template rendering support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+	// static resources.
+	app.useStaticAssets(join(__dirname, '..', 'public'));
+
+	// handlebars setup.
+	app.setBaseViewsDir(join(__dirname, '..', 'Views'));
+	hbs.registerPartials(join(__dirname, '..', 'Views', 'partials'));
+
+	// setting view engine as handlebars.
+	app.setViewEngine('hbs');
+
 	await app.listen(3000);
 }
 bootstrap();
